107592202d029696c025f56728b1865a77940457
juliafarry <83032550+juliafarry@users.noreply.github.com>
2021-04-22 14:02:17 -0400

    Add files via upload

---
+++ FinalProject.py
@@ -0,0 +1,15 @@
+"""
+At least one function that has two parameters and returns a value
+At least one function that does not return a value
+Interacting with dictionaries, lists, and tuples
+Using a Python module to calculate a statistical function such as average, median, mode, etc.
+User Interface and dashboard with Streamlit.io
+
+at least 3 pandas capabilities:
+Sorting data in ascending or descending order, multi-column sorting
+Filtering data by one or more conditions
+Analyzing data with pivot tables
+Managing rows or columns
+Add/drop/select/create new/group columns, frequency count, other features as you wish
+
+"""
